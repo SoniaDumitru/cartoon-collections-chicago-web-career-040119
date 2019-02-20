80ce983ec44a25ac7d6e094f4785ec05d1d0c440
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-20 18:19:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # The method should tell us if any of the calls 
+  # The method should tell us if any of the calls
   # are longer than four characters.
   array.each do |word|
   if word.length > 4
